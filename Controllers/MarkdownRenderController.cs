fb3d3765f2591223d155573847a0b53b58be93ba
stefanhug <stefan@huggle.org>
2021-04-19 19:56:26 +0200

    added markdown2html for readme.md

---
+++ Controllers/MarkdownRenderController.cs
@@ -0,0 +1,37 @@
+﻿using GenericDBRestApi.Managers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+
+namespace testwebapi.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.ApiController]
+    [Route("index.html")]
+    public class MarkdownRenderController : ControllerBase
+    {
+        private readonly ILogger<MarkdownRenderController> logger;
+        private readonly MarkdownRenderManager manager;
+       
+        public MarkdownRenderController(ILogger<MarkdownRenderController> logger, MarkdownRenderManager manager)
+        {
+            this.logger = logger;
+            this.manager = manager;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new ContentResult()
+            {
+                Content = manager.GetMdFileAsHtml("README.md"),
+                ContentType = "text/html",
+                StatusCode = (int)HttpStatusCode.OK
+            };
+        }
+    }
+}
---
+++ Managers/MarkdownRenderManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using MarkdownSharp;
+using Microsoft.Extensions.Logging;
+
+namespace GenericDBRestApi.Managers
+{
+    public class MarkdownRenderManager
+    {
+        private readonly ILogger<MarkdownRenderManager> logger;
+        private readonly Markdown markdown;
+
+        public MarkdownRenderManager(ILogger<MarkdownRenderManager> logger)
+        {
+            this.logger = logger;
+            markdown = new Markdown();
+        }
+
+        public string GetMdFileAsHtml(string fileName)
+        {
+            string content = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, fileName));
+            return markdown.Transform(content);
+        }
+    }
+}
