c75dfaea1f910a40927a5591920e7fd5a3b1aacb
stefanhug <stefan@huggle.org>
2021-05-16 18:47:22 +0200

    added razorlight engine for html output

---
+++ GenericDbRestApi.Lib/Formatters/QueryHtmlFormatter.cs
@@ -0,0 +1,27 @@
+﻿using GenericDbRestApi.Lib.Formatters;
+using GenericDbRestApi.Lib.Types;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace GenericDBRestApi.Lib.Formatters
+{
+    public class QueryHtmlFormatter : IQueryFormatter
+    {
+        public string ContentType => "text/html";
+        public string OutputFormat => "html";
+
+        public ActionResult GetActionResult(QueryResult queryResult)
+        {
+            var generator = new DbQueryHtml(queryResult);
+            return new ContentResult()
+            {
+                Content = generator.GetAsString(),
+                ContentType = this.ContentType,
+                StatusCode = (int)HttpStatusCode.OK
+            };
+        }
+    }
+}
--- GenericDbRestApi.Lib/Types/GenericDbQueryException.cs
+++ GenericDbRestApi.Lib/Types/GenericDbQueryException.cs
@@ -4,10 +4,12 @@ using System.Text;
 
 namespace GenericDbRestApi.Lib.Types
 {
-    public enum GenericDbQueryExceptionCode { QUERY_NOTFOUND, RECURSION, DBQUERY, FORMATTER_NOTFOUND,
-        PARAMS_MISSING,
-        PARAMS_NOTNEEDED
+    public enum GenericDbQueryExceptionCode
+    {
+        QUERY_NOTFOUND, RECURSION, DBQUERY, FORMATTER_NOTFOUND,
+        PARAMS_MISSING, PARAMS_NOTNEEDED, TEMPLATE_ERROR
     }
+
     public class GenericDbQueryException : Exception
     {
         public GenericDbQueryException(GenericDbQueryExceptionCode code, string message) : base(message) 
