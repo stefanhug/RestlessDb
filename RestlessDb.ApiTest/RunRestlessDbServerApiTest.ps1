63dec8a083f8e6063ba45948efe835e0f2bad999
stefanhug <stefan@huggle.org>
2021-06-24 18:18:58 +0200

    refactorings

--- RestlessDb.ApiTest/RunRestlessDbServerApiTest.ps1
+++ RestlessDb.ApiTest/RunRestlessDbServerApiTest.ps1
@@ -1,2 +1,4 @@
-﻿cd ../../../../bin/netcoreapp3.1
+﻿$MyDir = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
+cd $MyDir/../../../../bin/netcoreapp3.1
 ./RestlessDb.App.exe --environment=Development
+cd $MyDir
