84f663853131f818ba36a386eb2e79f0d9e57b39
stefanhug <stefan@huggle.org>
2022-01-04 18:25:19 +0100

    Mudblazor Table for embedded table display, Theming, Loading screen

---
+++ RestlessDb.Client/Shared/QueryResultTable.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using RestlessDb.Common.Types;
+using System;
+
+namespace RestlessDb.Client.Shared
+{
+    public partial class QueryResultTable
+    {
+        [Parameter]
+        public QueryResult QueryResult { get; set; }
+
+        public string TableLabel { get => QueryResult == null ? string.Empty : $"{QueryResult.MetaData.Label} - {QueryResult.MetaData.Description}"; }
+        public string TableDescription
+        {
+            get
+            {
+                if (QueryResult == null)    
+                    return string.Empty;
+
+                return $"Showing {QueryResult.RetrievedRows} rows" + (QueryResult.HasMoreRows ? "More rows available" : string.Empty);
+            }
+        }
+    }
+}
